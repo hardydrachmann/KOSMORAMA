5d379555dc7239afa614cfd26a80e6d6cecfac80
Dennis Petersen <dennp7791@gmail.com>
2016-08-04 07:57:32 +0000

    youtube player added to training instruction

--- www/app/trainingInstruction/trainingInstructionController.js
+++ www/app/trainingInstruction/trainingInstructionController.js
@@ -1,3 +1,3 @@
-angular.module('kosmoramaApp').controller('TrainingInstructionController', function($scope, $state) {
-  $scope.data = 'data data data';
+angular.module('kosmoramaApp').controller('TrainingInstructionController', function($scope, $state, $sce) {
+  $scope.video= $sce.trustAsResourceUrl('https://www.youtube.com/embed/hsc2bhCW80Y?autoplay=1&loop=1');
 });
