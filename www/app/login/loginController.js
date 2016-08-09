91a1ecfd8a44cc75880b686ba340214bc01d9fc1
hardydrachmann <hardydrachmann@phoebuscartel.dk>
2016-08-09 12:46:59 +0200

    da+en tekster rettet og tilføjet hvor nødvendigt, samt popupservice forbedret med grafiske knapper

--- www/app/login/loginController.js
+++ www/app/login/loginController.js
@@ -27,28 +27,29 @@ angular.module('kosmoramaApp').controller('LoginController', function($scope, $s
 
   $scope.login = function() {
     if ($scope.userScreenNumber) {
+      $scope.showLoading();
       dataService.getUser($scope.userScreenNumber, function(result) {
-        if (result) {
-          $scope.showLoading();
+        if (result.length > 0) {
           var key = $scope.getRandomKey();
           var id = sjcl.encrypt(key, $scope.userScreenNumber);
           window.localStorage.setItem('kosmoramaId', id);
           window.localStorage.setItem('kosmoramaKey', key);
           $('#setUserScreenNumber').val('');
-          $timeout(function() {
-            $scope.setTabs();
-            $scope.hideLoading();
-            $state.go('home');
-          }, 2000);
+          $scope.setTabs();
+          $scope.hideLoading();
+          $state.go('home');
         } else {
-          popupService.popup($scope.getText('loginHelp'), 5000);
+          $scope.hideLoading();
+          popupService.AlertPopup($scope.getText('loginFail'));
         }
       });
+    } else {
+      popupService.AlertPopup($scope.getText('loginHelp'));
     }
   };
 
   $scope.logout = function() {
-    popupService.confirmPopup('Logout', '', function() {
+    popupService.confirmPopup($scope.getText('logoutText') + '?', '', function() {
       window.localStorage.removeItem('kosmoramaId');
       window.localStorage.removeItem('kosmoramaKey');
       $state.go('login');
