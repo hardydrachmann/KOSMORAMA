468c91b9870383c5d5019f3c653b9c10a81d03bf
hardydrachmann <hardydrachmann@phoebuscartel.dk>
2016-08-11 09:37:40 +0200

    added loading screen popups across the app

--- www/app/home/homeController.js
+++ www/app/home/homeController.js
@@ -1,42 +1,43 @@
-angular.module('kosmoramaApp').controller('HomeController', function($scope, $state, $ionicHistory, dataService) {
-    $scope.mails = [];
-    $scope.hasMail = false;
+angular.module('kosmoramaApp').controller('HomeController', function($scope, $state, $ionicHistory, dataService, loadingService) {
+  $scope.mails = [];
+  $scope.hasMail = false;
 
-    var getMails = function() {
-        // Produce mock data.
-        for (var i = 0; i < 10; i++) {
-            $scope.mails.push({
-                title: 'title' + i,
-                message: 'message' + i,
-                timestamp: '2016',
-                isRead: false
-            });
-        }
-        $scope.hasMail = $scope.mails.length > 0;
-    };
-    getMails();
+  var getMails = function() {
+    // Produce mock data.
+    loadingService.loaderShow();
+    for (var i = 0; i < 10; i++) {
+      $scope.mails.push({
+        title: 'title' + i,
+        message: 'message' + i,
+        timestamp: '2016',
+        isRead: false
+      });
+    }
+    loadingService.loaderHide();
+    $scope.hasMail = $scope.mails.length > 0;
+  };
+  getMails();
 
-    $scope.closeMailView = function() {
-        $state.go($ionicHistory.backView().stateName);
-    };
+  $scope.closeMailView = function() {
+    $state.go($ionicHistory.backView().stateName);
+  };
 
-    $scope.toggleMailDisplay = function(index) {
-        var mail = $('#mail' + index);
-        if (mail.hasClass('inactive-mail')) {
-            mail.removeClass('inactive-mail');
-            mail.addClass('active-mail');
-        }
-        else {
-            mail.removeClass('active-mail');
-            mail.addClass('inactive-mail');
-        }
-    };
+  $scope.toggleMailDisplay = function(index) {
+    var mail = $('#mail' + index);
+    if (mail.hasClass('inactive-mail')) {
+      mail.removeClass('inactive-mail');
+      mail.addClass('active-mail');
+    } else {
+      mail.removeClass('active-mail');
+      mail.addClass('inactive-mail');
+    }
+  };
 
-    $scope.logMail = function(index) {
-        console.log($scope.mails[index]);
-    };
+  $scope.logMail = function(index) {
+    console.log($scope.mails[index]);
+  };
 
-    $scope.newMail = function() {
-        return '';
-    };
+  $scope.newMail = function() {
+    return '';
+  };
 });
--- www/app/training/trainingController.js
+++ www/app/training/trainingController.js
@@ -1,96 +1,97 @@
 var app = angular.module('kosmoramaApp');
-app.controller('TrainingController', function($scope, $state, $sce, $timeout, dataService) {
+app.controller('TrainingController', function($scope, $state, $sce, $timeout, dataService, loadingService) {
 
-    $scope.TrainingItems = [];
+  $scope.TrainingItems = [];
 
-    var getTraining = function(userId) {
-        dataService.getTraining(userId, function(data) {
-            if (data.length > 0) {
-                $scope.TrainingItems = data[0].TrainingItems;
-            }
-        });
-    };
-    getTraining(79);
+  var getTraining = function(userId) {
+    loadingService.loaderShow();
+    dataService.getTraining(userId, function(data) {
+      if (data.length > 0) {
+        $scope.TrainingItems = data[0].TrainingItems;
+      }
+      loadingService.loaderHide();
+    });
+  };
+  getTraining(79);
 
-    $scope.getTrainingName = function(trainingItem) {
-        // Returns the appropriate language name for the selected item.
-        return trainingItem.LangName[$scope.lang];
-    };
+  $scope.getTrainingName = function(trainingItem) {
+    // Returns the appropriate language name for the selected item.
+    return trainingItem.LangName[$scope.lang];
+  };
 
-    $scope.trainingDescription = function() {
-        // Returns the appropriate language description for the next exercise.
-        var item = $scope.TrainingItems[0];
-        if (item != undefined) {
-            return item.LangDesc[$scope.lang];
+  $scope.trainingDescription = function() {
+    // Returns the appropriate language description for the next exercise.
+    var item = $scope.TrainingItems[0];
+    if (item != undefined) {
+      return item.LangDesc[$scope.lang];
+    }
+  };
+
+  $scope.getVideo = function() {
+    var item = $scope.TrainingItems[0];
+    if (item != undefined) {
+      var url = item.ExeciseUrl;
+      if (url) {
+        return $sce.trustAsResourceUrl('https://www.youtube.com/embed/xx2cxo8WQoM?rel=0&showinfo=0');
+        //return $sce.trustAsResourceUrl(url + '/embed/xx2cxo8WQoM?rel=0&showinfo=0');
+      }
+    }
+  };
+
+  var url = 'https://welfaredenmark.blob.core.windows.net/exercises/Exercises/';
+  var urn = '/picture/picture.png';
+  $scope.getPicture = function(exerciseId) {
+    return url + exerciseId + urn;
+  };
+
+  // Timer stuff for video playback.
+  var mytimeout = null;
+  var rep;
+  var timerep;
+  var timepause;
+  var pause = false;
+  $scope.counter = null;
+
+  $scope.formatTime = function(time) {
+    var min = Math.floor(time / 60);
+    var sec = time - min * 60;
+    return min + " minutes " + sec + " seconds"
+  };
+
+  $scope.timer = function() {
+    rep = $scope.TrainingItems[0].Repetitions;
+    timerep = $scope.TrainingItems[0].TimeSet * 60;
+    timepause = $scope.TrainingItems[0].Pause * 60;
+    console.log("Repetitions: " + rep);
+    console.log("Time pr rep: " + timerep);
+    $scope.counter = timerep;
+  };
+
+  $scope.onTimeout = function() {
+    if ($scope.counter === 0) {
+      $timeout.cancel(mytimeout);
+      if (rep > 0)
+        if (!pause) {
+          $scope.startExcerciseTimer();
+        } else {
+          $scope.startPauseTimer();
         }
-    };
+      return;
+    }
+    $scope.counter--;
+    mytimeout = $timeout($scope.onTimeout, 1000);
+  };
 
-    $scope.getVideo = function() {
-        var item = $scope.TrainingItems[0];
-        if (item != undefined) {
-            var url = item.ExeciseUrl;
-            if (url) {
-                return $sce.trustAsResourceUrl('https://www.youtube.com/embed/xx2cxo8WQoM?rel=0&showinfo=0');
-                //return $sce.trustAsResourceUrl(url + '/embed/xx2cxo8WQoM?rel=0&showinfo=0');
-            }
-        }
-    };
+  $scope.startExcerciseTimer = function() {
+    $scope.counter = timerep + 1;
+    pause = true;
+    rep--;
+    mytimeout = $timeout($scope.onTimeout);
+  };
 
-    var url = 'https://welfaredenmark.blob.core.windows.net/exercises/Exercises/';
-    var urn = '/picture/picture.png';
-    $scope.getPicture = function(exerciseId) {
-        return url + exerciseId + urn;
-    };
-
-    // Timer stuff for video playback.
-    var mytimeout = null;
-    var rep;
-    var timerep;
-    var timepause;
-    var pause = false;
-    $scope.counter = null;
-
-    $scope.formatTime = function(time) {
-        var min = Math.floor(time / 60);
-        var sec = time - min * 60;
-        return min + " minutes " + sec + " seconds"
-    };
-
-    $scope.timer = function() {
-        rep = $scope.TrainingItems[0].Repetitions;
-        timerep = $scope.TrainingItems[0].TimeSet * 60;
-        timepause = $scope.TrainingItems[0].Pause * 60;
-        console.log("Repetitions: " + rep);
-        console.log("Time pr rep: " + timerep);
-        $scope.counter = timerep;
-    };
-
-    $scope.onTimeout = function() {
-        if ($scope.counter === 0) {
-            $timeout.cancel(mytimeout);
-            if (rep > 0)
-                if (!pause) {
-                    $scope.startExcerciseTimer();
-                }
-                else {
-                    $scope.startPauseTimer();
-                }
-            return;
-        }
-        $scope.counter--;
-        mytimeout = $timeout($scope.onTimeout, 1000);
-    };
-
-    $scope.startExcerciseTimer = function() {
-        $scope.counter = timerep + 1;
-        pause = true;
-        rep--;
-        mytimeout = $timeout($scope.onTimeout);
-    };
-
-    $scope.startPauseTimer = function() {
-        $scope.counter = timepause + 1;
-        pause = false;
-        mytimeout = $timeout($scope.onTimeout);
-    };
+  $scope.startPauseTimer = function() {
+    $scope.counter = timepause + 1;
+    pause = false;
+    mytimeout = $timeout($scope.onTimeout);
+  };
 });
