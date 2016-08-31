534f8fe2ea7938fc6367dd5ecda7f9a59983be0e
Rasmus Nielsen <rasmniel@gmail.com>
2016-08-31 07:31:41 +0000

    stash

--- www/app/services/storageService.js
+++ www/app/services/storageService.js
@@ -1,15 +1,16 @@
-
 angular.module('kosmoramaApp').service('storageService', function($http) {
+    this.persistentUserData = {userScreenNumber: 'aa10', training: []};
 
+    this.setKey = function(key, value) {
+        return window.localStorage.setItem(key, value);
+    };
 
-this.setKey = function(key,value){
-    return window.localStorage.setItem(key, value);
-}
-this.getKey = function(key){
-    return window.localStorage.getItem(key);
-}
-this.destroy = function(key){
-    return window.localStorage.removeItem(key);
-}
+    this.getKey = function(key) {
+        return window.localStorage.getItem(key);
+    };
+
+    this.destroy = function(key) {
+        return window.localStorage.removeItem(key);
+    };
 
 });
