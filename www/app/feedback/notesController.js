4950675d253bce7669830d026264a7581e394963
hardydrachmann <hardydrachmann@phoebuscartel.dk>
2016-08-31 09:40:44 +0200

    various fixes and optimization

--- www/app/feedback/notesController.js
+++ www/app/feedback/notesController.js
@@ -1,43 +1,56 @@
 angular.module('kosmoramaApp').controller('NotesController', function($scope, $state, $rootScope, $ionicHistory, dataService) {
 
-    $scope.painValue = '';
-    $scope.messageText = '';
+	$scope.painValue = '';
+	$scope.messageText = '';
 
-    $(document).ready(function() {
-        console.log($rootScope.passData);
-        var handler = $rootScope.$on('continueEvent', function() {
-            if ($scope.painValue && $rootScope.passData.painLevel === null) {
-                $rootScope.passData.painLevel = $scope.painValue;
-            }
-            else if ($scope.messageText && $rootScope.passData.message === null) {
-                $rootScope.passData.message = $scope.messageText;
-            }
-            $rootScope.lastPassTraining = false;
-            $rootScope.currentTraining = {};
-            if ($rootScope.passData && $ionicHistory.currentView().stateName === 'notes') {
-                dataService.postFeedback($rootScope.passData);
-                $rootScope.passData = null;
-            }
-            handler();
-        });
-    });
+	$(document).ready(main);
 
-    $scope.clearContent = function(event) {
-        var element = $(event.target);
-        element.attr('placeholder', '');
-    };
+	/**
+	 * Gets the data from the current view and puts the data into the rootscope. 
+	 */
+	var main = function() {
+		console.log($rootScope.passData);
+		var handler = $rootScope.$on('continueEvent', function() {
+			if ($scope.painValue && $rootScope.passData.painLevel === null) {
+				$rootScope.passData.painLevel = $scope.painValue;
+			} else if ($scope.messageText && $rootScope.passData.message === null) {
+				$rootScope.passData.message = $scope.messageText;
+			}
+			$rootScope.lastPassTraining = false;
+			$rootScope.currentTraining = {};
+			if ($rootScope.passData && $ionicHistory.currentView().stateName === 'notes') {
+				dataService.postFeedback($rootScope.passData);
+				$rootScope.passData = null;
+			}
+			handler();
+		});
+	};
 
-    $scope.revertContent = function(event) {
-        var element = $(event.target);
-        element.attr('placeholder', $scope.getText('notesTitle'));
-    };
+	/**
+	 * When the text area is entered, clear the placeholder text.
+	 */
+	$scope.clearContent = function(event) {
+		var element = $(event.target);
+		element.attr('placeholder', '');
+	};
 
-    $scope.maxChars = 500;
-    $scope.messageText = '';
-    $scope.remainingChars = function() {
-        var textLength = $scope.messageText.length;
-        var remainingChars = $scope.maxChars - textLength;
-        $('#showCharsCount').html(remainingChars);
-    };
+	/**
+	 * When no text is entered into the text area when entered, and you leave, insert the default placeholder text once again.
+	 */
+	$scope.revertContent = function(event) {
+		var element = $(event.target);
+		element.attr('placeholder', $scope.getText('notesTitle'));
+	};
+
+	/**
+	 * Display remaining amount of characters to the user (max is set at 500).
+	 */
+	$scope.maxChars = 500;
+	$scope.messageText = '';
+	$scope.remainingChars = function() {
+		var textLength = $scope.messageText.length;
+		var remainingChars = $scope.maxChars - textLength;
+		$('#showCharsCount').html(remainingChars);
+	};
 
 });
--- www/app/services/popupService.js
+++ www/app/services/popupService.js
@@ -2,45 +2,47 @@
 
 angular.module('kosmoramaApp').service('popupService', function($ionicPopup, $timeout) {
 
-  this.popup = function(message, time) {
-    var popup = $ionicPopup.show({
-      template: message
-    });
-    $timeout(function() {
-      popup.close();
-    }, time || 2000);
-  };
+	/**
+	 * Initiates a default popup when called.
+	 */
+	this.popup = function(message, time) {
+		var popup = $ionicPopup.show({
+			template: message
+		});
+		$timeout(function() {
+			popup.close();
+		}, time || 2000);
+	};
 
-  /**
-   * Initiates a Popup when called.
-   */
-  this.confirmPopup = function(title, toConfirm, callback) {
-    var confirm = $ionicPopup.confirm({
-      title: title,
-      template: toConfirm,
-      okText: ' ',
-      okType: 'button icon-center ion-ios-checkmark-outline button-balanced',
-      cancelText: ' ',
-      cancelType: 'button icon-center ion-ios-close-outline button-assertive'
-    });
+	/**
+	 * Initiates a confirm popup when called.
+	 */
+	this.confirmPopup = function(title, toConfirm, callback) {
+		var confirm = $ionicPopup.confirm({
+			title: title,
+			template: toConfirm,
+			okText: ' ',
+			okType: 'button icon-center ion-ios-checkmark-outline button-balanced',
+			cancelText: ' ',
+			cancelType: 'button icon-center ion-ios-close-outline button-assertive'
+		});
+		confirm.then(function(response) {
+			if (response) {
+				callback();
+			}
+		});
+	};
 
-    confirm.then(function(response) {
-      if (response) {
-        callback();
-      }
-    });
-  };
-
-  /**
-   * Alerts Popup when called.
-   */
-  this.alertPopup = function(message) {
-    var alert = $ionicPopup.alert({
-      template: message,
-      buttons: [{
-        type: 'button icon-center ion-ios-checkmark-outline button-balanced'
-      }]
-    });
-  };
+	/**
+	 * Initiates an alert popup when called.
+	 */
+	this.alertPopup = function(message) {
+		var alert = $ionicPopup.alert({
+			template: message,
+			buttons: [{
+				type: 'button icon-center ion-ios-checkmark-outline button-balanced'
+			}]
+		});
+	};
 
 });
