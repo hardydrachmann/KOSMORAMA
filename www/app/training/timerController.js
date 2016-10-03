7561edd3c6d3c195460a0d5bd10eb0a9ff395644
Rasmus Nielsen <rasmniel@gmail.com>
2016-10-03 06:56:29 +0000

    Training...

--- www/app/training/timerController.js
+++ www/app/training/timerController.js
@@ -3,6 +3,7 @@ angular
 	.controller('TimerController', function($interval, $window, $timeout, $state, $ionicHistory, languageService, storageService, mediaService, tabsService) {
 
 		var self = this;
+
 		var counter;
 		var video = $('video').get(0);
 
@@ -11,6 +12,7 @@ angular
 		self.paused = false;
 		self.radius = 0;
 		self.intermission = false;
+
 		self.training = {
 			sets: 0,
 			time: 0,
@@ -21,8 +23,8 @@ angular
 			var currentTraining = storageService.proceduralUserData.currentTraining;
 			self.training = {
 				sets: currentTraining.Sets,
-				time: currentTraining.TimeSet * 60,
-				pause: currentTraining.Pause * 60
+				time: (currentTraining.TimeSet * 60) + 1,
+				pause: (currentTraining.Pause * 60) + 1
 			};
 			$window.onresize = refreshRadius;
 			refreshRadius();
@@ -33,36 +35,26 @@ angular
 		 * Pause the timer if not in intermission. If paused, resume instead.
 		 */
 		self.pause = function() {
-			console.log(video);
-			if (!self.intermission) {
-				if (self.paused) {
-					self.resume();
-					self.paused = false;
-				}
-				else {
-					$interval.cancel(counter);
-					counter = null;
-					$timeout(function() {
-						// Let interval cancel before pausing video.
-						video.pause();
-					}, 100);
-					self.paused = true;
-				}
-			}
-		};
-
-		/**
-		 * Resume the timer if training is not in intermission.
-		 */
-		self.resume = function() {
-			video.play();
-			// Get remaining seconds point before starting inc/dec again.
-			var seconds = self.training.time - self.seconds;
-			incrementTimer();
-			counter = $interval(function() {
+			if (self.paused) {
+				// Get remaining seconds before starting incrementing again.
+				var seconds = self.training.time - self.seconds;
+				// Start incrementing.
 				incrementTimer();
-			}, 1000, seconds);
-
+				counter = $interval(function() {
+					incrementTimer();
+				}, 1000, seconds);
+				self.paused = false;
+				video.play();
+			}
+			else {
+				$interval.cancel(counter);
+				counter = null;
+				self.paused = true;
+				// Let interval cancel before pausing video.
+				$timeout(function() {
+					video.pause();
+				}, 100);
+			}
 		};
 
 		/**
@@ -88,47 +80,60 @@ angular
 			};
 		};
 
+		self.getIcon = function() {
+			return self.paused ? 'ion-play icon-position' : 'ion-pause';
+		};
+
 		/**
 		 * Start the timer if not already started.
 		 */
 		function start(time) {
 			self.capacity = time;
 			self.seconds = self.intermission ? time : 0;
-			video.play();
 			incrementTimer();
 			counter = $interval(function() {
 				incrementTimer();
 			}, 1000, time);
+			video.play();
 		}
 
 		/**
+		 * Increments the training timer or decrements the intermission timer. Also toggles between the two states.
 		 * If training in progress, increment the timer it by a second until the specified time is reached.
 		 * If pause in progress, decrement the timer by a second until 0 is reached.
 		 * When the limit is reached, switch between active and pause. Decrement set count, if pause is over.
 		 */
 		function incrementTimer() {
+			// If there is an intermission.
 			if (self.intermission) {
+				// If there's still time left.
 				if (self.seconds > 0) {
 					self.seconds--;
 				}
+				// If the intermission has just ended.
 				else {
 					self.intermission = false;
 					start(self.training.time);
 					video.play();
+					// If there are any more sets left.
 					if (self.training.sets > 0) {
 						self.training.sets--;
 					}
 				}
 			}
+			// If training is in progress.
 			else {
+				// If there is time left.
 				if (self.seconds < self.training.time) {
 					self.seconds++;
 				}
+				// If the set just ended, and there are any sets left.
 				else if (self.training.sets > 1) {
 					self.intermission = true;
 					start(self.training.pause);
 					video.pause();
 				}
+				// If training is done.
 				else {
 					self.reset();
 					tabsService.continue();
